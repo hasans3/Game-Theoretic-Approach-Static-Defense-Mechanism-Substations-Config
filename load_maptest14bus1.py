ca1226274442abdfcb6dee983025360efae9af4e
hasans3 <saqib.hasan@vanderbilt.edu>
2018-06-02 12:57:40 -0500

    Update load_maptest14bus1.py

--- load_maptest14bus1.py
+++ load_maptest14bus1.py
@@ -4,22 +4,19 @@ Created on Tue Oct 04 11:01:35 2016
 
 @author: saqibhasan
 
-This is the function to calcaulate the load loss given a contingency.
-It is used in cascade_algorithm.py
+This is the method to compute the load loss given a contingency. It is used in cascade_algorithm.py
 
 """
 
 def load_maptest14bus(load_file_name, load_loss):
     
-#    keyset1 = {'Load.load1': 21.7,'Load.load2': 7.6,'Load.load3': 11.2,'Load.load4': 6.1,'Load.load5': 13.8,'Load.load6': 3.5,'Load.load7': 14.9,'Load.load8': 9,'Load.load9': 16.6,'Load.load10': 47.8,'Load.load11': 94.2};
 # -------------- Open and read the text file and convert the content into a dictionary ----------------
-#    keyset1 = {'Load.load2': (7600,1600),'Load.load5': (13800,5800),'Load.load8': (9000,5800),'Load.load9': (29500,16600),'Load.load3': (11200,7500),'Load.load11': (94200,19100),'Load.load4': (6100,1600),'Load.load6': (3500,1800),'Load.load1': (21700,12700),'Load.load10': (47800,-3900),'Load.load7': (14900,5000)}
     file_data = open(load_file_name, 'r')
     data = file_data.readline();
     keyset1 = eval(data);
     file_data.close()
-    #load_loss = ['Load.load2','Load.load5'];
     load_loss1 = load_loss;
+# ---------------- Import necessary method from library and variables initialization ----------------
     import math
     Total_system_load = 0.0;
     load_loss_values = [];
@@ -34,8 +31,7 @@ def load_maptest14bus(load_file_name, load_loss):
             c = c+b;
         d = math.sqrt(c);
         Total_system_load = Total_system_load + d;
-# ----------------- Getting the values of the keys from dictionary ------------------
-#    load_loss_size = len(load_loss1);
+# ----------------- Getting the loads that are removed from the system ------------------
     load_loss_size = len(load_loss1);
     for i in range(0, load_loss_size):
         load_loss_values.append(keyset1[load_loss1[i]]);
