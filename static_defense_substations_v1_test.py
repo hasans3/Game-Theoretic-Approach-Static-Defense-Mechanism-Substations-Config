6fb592f7e5a2d2ade844e31ffaaa8cc7e136602b
hasans3 <saqib.hasan@vanderbilt.edu>
2018-06-02 12:40:03 -0500

    Update static_defense_substations_v1_test.py

--- static_defense_substations_v1_test.py
+++ static_defense_substations_v1_test.py
@@ -9,6 +9,8 @@ named as version1.x
 """
 
 def static_defense_subs(filepath, comp_filename, load_file_name, start_range, contingency_range, blackout_criterion, system_name, p_budget, s_budget, d_budget):
+
+    # Importing the supporting methods 
     import time
     import static_attack_subs_v2
     import static_defense_substations_v1_support
@@ -19,24 +21,32 @@ def static_defense_subs(filepath, comp_filename, load_file_name, start_range, co
     import static_defense_substations1_v1_support
     import post_defense_static_attack_subs
     
+    # Method variables initialization
     tot_exe_time_start = time.time()
     temp_sub_protected = [];
     sub_protected = [];
     sub_protected1 = [];
     temp_max_loadloss = 100;
     prev_post_defese_loadloss = 100;
+    
+    # Identifying the worst case static attack and storing the details
     temp_worst_case_outage, temp_worst_case_loadloss, temp_worst_case_sub = static_attack_subs_v2.greedy_hueristics(filepath, comp_filename, load_file_name, start_range, contingency_range, blackout_criterion, system_name, p_budget, s_budget);
     pre_defense_worst_case_outage = temp_worst_case_outage;
     pre_defense_worst_case_loadloss = temp_worst_case_loadloss;
     pre_defense_worst_case_subs = temp_worst_case_sub;
+    
+    # Running the loop until the defense budget expires
     for i in range(0, d_budget):
         temp_max_loadloss = 100;
         flag = 0
+        # This check runns the if substations are protected to identify the new static attack
         if (len(sub_protected) != 0):
             temp_worst_case_sub, prev_post_defese_loadloss = static_defense_substations1_v1_support.greedy_hueristics(filepath, comp_filename, load_file_name, start_range, contingency_range, blackout_criterion, system_name, p_budget, s_budget, sub_protected);
             temp_worst_case_sub = temp_worst_case_sub;
+        # Removes each substation and its components from the attackable set and obtain the system damage
         for item in range(0, len(temp_worst_case_sub)):
             temp_worst_case_outage, temp_worst_case_loadloss, temp_worst_case_substation = static_defense_substations_v1_support.greedy_hueristics(filepath, comp_filename, load_file_name, start_range, contingency_range, blackout_criterion, system_name, p_budget, s_budget, tuple([temp_worst_case_sub[item]]), sub_protected);
+            # Updates the solution for the substation that minimizes the damage if protected
             if (temp_worst_case_loadloss < temp_max_loadloss):
                 temp_max_loadloss = temp_worst_case_loadloss
 #                prev_post_defese_loadloss = temp_worst_case_loadloss
@@ -44,6 +54,7 @@ def static_defense_subs(filepath, comp_filename, load_file_name, start_range, co
                 flag = 1
         sub_protected.append(temp_sub_protected); 
         sub_protected1.append(temp_sub_protected);
+        # Keeps a temporary solution in order to obtain an optimal solution
         if (temp_max_loadloss > prev_post_defese_loadloss and flag == 1):
             sub_protected1.remove(temp_sub_protected);
         else:
@@ -61,9 +72,13 @@ def static_defense_subs(filepath, comp_filename, load_file_name, start_range, co
 #    print sub_protected
 #        prev_post_defese_loadloss = post_defense_worst_case_loadloss
 #        print sub_protected 
+
+    # Obtain the worst case static attack after protecting the substations based on the defense budget
     post_defense_worst_case_outage, post_defense_worst_case_sub, post_defense_worst_case_loadloss = post_defense_static_attack_subs.greedy_hueristics(filepath, comp_filename, load_file_name, start_range, contingency_range, blackout_criterion, system_name, p_budget, s_budget, sub_protected1);
     tot_exe_time_end = time.time()
     tot_exe_time = (tot_exe_time_end - tot_exe_time_start)    
+    
+    # Outputs the details on the console
     print '**************************************************************************************************************************'
     print 'Pre defense worst case static-attack outages: %s' %pre_defense_worst_case_outage
     print 'Pre defense worst case static-attack on substations: {0}'.format(pre_defense_worst_case_subs)
@@ -75,7 +90,8 @@ def static_defense_subs(filepath, comp_filename, load_file_name, start_range, co
     print 'Post defense percentage reduction in load loss: {0}%'.format(((pre_defense_worst_case_loadloss - post_defense_worst_case_loadloss)/pre_defense_worst_case_loadloss)*100)
     print 'Total execution time in seconds: %s' %tot_exe_time
     print '**************************************************************************************************************************'
-    
+
+# Method call with different system models
 static_defense_subs("'G:\saqib\open DSS\OpenDSS_Python_Interface\ieee_14_bus_data\ieee14bus_system_with_txr.dss'", "G:\saqib\open DSS\OpenDSS_Python_Interface\ieee_14_bus_data\component_data_subs.txt", "G:\saqib\open DSS\OpenDSS_Python_Interface\Load_data_with_reactive_power.txt", 0, 1, 40, 'ieee14bus_system_N-1.xml', 3, 3, 5);
 #static_defense_subs("'G:\saqib\open DSS\OpenDSS_Python_Interface\ieee39bus_system\ieee39bus_system_with_1kv_base.dss'", "G:\saqib\open DSS\OpenDSS_Python_Interface\ieee39bus_system\component_data_subs.txt", "G:\saqib\open DSS\OpenDSS_Python_Interface\ieee39bus_system\Load_data1.txt", 0, 1, 40, 'ieee39bus_system_test_N-3.xml', 2, 2, 14);
 #static_defense_subs("'G:\saqib\open DSS\OpenDSS_Python_Interface\ieee_57_bus_system\ieee57bussystem1.dss'", "G:\saqib\open DSS\OpenDSS_Python_Interface\ieee_57_bus_system\component_data_subs.txt", "G:\saqib\open DSS\OpenDSS_Python_Interface\ieee_57_bus_system\Load_data.txt", 0, 1, 40, 'ieee57bus_system_N-1.xml', 2, 2, 4);    
