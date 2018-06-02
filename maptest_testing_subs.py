fa634513515ef55750cf205c8b8ab8c5f8928b53
hasans3 <saqib.hasan@vanderbilt.edu>
2018-06-02 13:06:56 -0500

    Update maptest_testing_subs.py

--- maptest_testing_subs.py
+++ maptest_testing_subs.py
@@ -6,20 +6,18 @@ Created on Tue Jun 06 15:45:48 2017
 """
 def maptest14bus_test_system(comp_filename, start_range, contingency_range):
     
-# To generate all possible combinations itertools is needed
+# Method variables initialization
     import itertools
     cmb = [];
     valueset =[];
     valueset = comp_filename;
-#    print valueset
-# ------------- Generate all the possible combinations from the list ------------- 
+
+# Converting the contingencies into the input format of the required method
     for i in range(start_range, contingency_range):
         comb=[];
         for j in itertools.combinations(valueset, i+1):
             comb.append(list(j));
-            #print comb
         cmb.append(comb);
-#    print cmb
     return cmb;
     
 #maptest14bus_test_system("G:\saqib\open DSS\OpenDSS_Python_Interface\ieee_14_bus_data\component_data_subs.txt",1,2);
