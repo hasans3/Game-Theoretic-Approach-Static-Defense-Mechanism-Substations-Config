940d573825bb9291da4842a7c7b93bff498ddc73
hasans3 <saqib.hasan@vanderbilt.edu>
2018-06-02 13:04:13 -0500

    Update maptest_testing.py

--- maptest_testing.py
+++ maptest_testing.py
@@ -7,7 +7,7 @@ Created on Tue Mar 14 16:21:26 2017
 def maptest14bus_test_system(comp_filename, start_range, contingency_range, substation_name, pro_subs):
     
     valueset = [];
-# -------------- Open and read the text file and convert the content into a list ----------------    
+# -------------- Open and read the text file and convert the content into a dictionary ----------------    
     data_file = open(comp_filename, 'r'); 
     line_data = data_file.readline();
     valueset = eval(line_data);
@@ -15,6 +15,8 @@ def maptest14bus_test_system(comp_filename, start_range, contingency_range, subs
     print valueset
     print len(valueset)
     print len(substation_name)
+    
+    # Remove substations from the system
     for item in range(0, len(pro_subs)):
         if pro_subs[item] in valueset:
             del valueset[pro_subs[item]];
